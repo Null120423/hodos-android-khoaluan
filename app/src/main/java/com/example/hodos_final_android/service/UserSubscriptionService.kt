4124e6dfbfc910ca86bf365b56f270e4ff8b0d44
huutai <huutt201@example.com>
2025-06-30 07:50:34 +0700

    feat: user subscription

---
+++ app/src/main/java/com/example/hodos_final_android/service/UserSubscriptionService.kt
@@ -0,0 +1,23 @@
+package com.example.hodos_final_android.service
+
+
+import com.example.hodos_final_android.model.PricingPlanModel
+import com.example.hodos_final_android.model.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+data class StartFreeTrialDTO(
+    val pricingPlanId: String
+)
+
+
+interface UserSubscriptionService {
+    @GET("mobile/pricing-plan/active-plan")
+    suspend fun getActivesPlan(): List<PricingPlanModel>
+
+    @POST("mobile/user-subscription/start-free-tria")
+    suspend fun startFreeTrial(@Body body: StartFreeTrialDTO): Response<Any>
+
+}
+
